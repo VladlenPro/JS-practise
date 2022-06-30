dcb8e07c0f873a84277b961cca0f5911c2089553
Vlad Probst <vladlen.probst@gmail.com>
2022-06-30 19:13:33 +0300

    delete unesesery logs and divede to functions

--- timestamp/index.js
+++ timestamp/index.js
@@ -7,19 +7,21 @@ let count = 0;
 function timeStamp() {
     if (time === null) {
         time = new Date()
-        console.log(time.toString());
     } else {
-        console.log(time);
         let timeDiffrance = Math.abs((new Date() - time) / 1000)
         timeLAnswer.innerText = `${timeDiffrance} Seconds`;
         timeSum += timeDiffrance;
         count++
         if (count === 10) {
-            let average = timeSum / count;
-            clickAverage.innerText = `everage of yourClicks is ${average} seconds`;
-            count = 0;
-            timeSum = 0;
+            clickTimeAverage();
         }
         time = null;
     }
+}
+
+function clickTimeAverage() {
+    let average = timeSum / count;
+    clickAverage.innerText = `average of your clicks is ${average} seconds`;
+    count = 0;
+    timeSum = 0;
 }
